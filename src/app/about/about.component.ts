2b459a5941dffcffc56178ba97233bd4ae82aa47
Parham Alvani <parham.alvani@gmail.com>
2018-12-21 23:43:59 +0330

    Removes redundant import.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.less']
 })
 export class AboutComponent implements OnInit {
+
+  @HostBinding('attr.class') elementClasses = ['content'];
 
   constructor() { }
 
