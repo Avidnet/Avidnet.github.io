5f3d54ec257ed2a4a8120381a10d3052c049aa87
Parham Alvani <parham.alvani@gmail.com>
2018-12-22 10:01:28 +0330

    Adds work section styles.

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Work {
+  title: string;
+  description: string;
+  href: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.less']
 })
 export class WorkComponent implements OnInit {
+
+  public works: Work[] = [
+    {
+      href: 'http://www.jovainco.com/',
+      title: 'Jovain Industrial &amp; Agricultural Co.',
+      description: '',
+      image: 'assets/img/works/w1.jpg'
+    }
+  ]
 
   constructor() { }
 
