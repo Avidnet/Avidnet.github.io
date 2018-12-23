e3410e3aba5b7ad6e985de257fc3611d3c99058d
Parham Alvani <parham.alvani@gmail.com>
2018-12-24 02:40:37 +0330

    Client slide show is ready for its duty.

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 
 interface Client {
-  show: boolean;
   name: string;
   title: string;
   quote: string;
@@ -17,10 +16,13 @@ export class ClientComponent implements OnInit {
   @HostBinding('attr.class') elementClasses = 'content has-bg bg-green';
 
   public clients: Client[] = [{
-    show: true,
     name: 'Joveyn',
     title: 'CEO',
     quote: 'Agriculture needs redesign <br> and Avid does it'
+  }, {
+    name: 'Magfa',
+    title: 'IoT Project Director',
+    quote: 'Create platform <br> when you need it'
   }];
 
   constructor() { }
