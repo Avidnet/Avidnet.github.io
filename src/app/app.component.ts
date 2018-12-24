9fd94144c37b1d2a36123943a729ee317cc14182
Parham Alvani <parham.alvani@gmail.com>
2018-12-25 02:18:52 +0330

    Support browser language detection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
   constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
 
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fa/) ? browserLang : 'en');
   }
 }
