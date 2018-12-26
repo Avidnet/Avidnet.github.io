4e9e27a73bbeb554d6383d800ec94110f4d4db5b
Parham Alvani <parham.alvani@gmail.com>
2018-12-26 17:49:28 +0330

    Nothing important.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
