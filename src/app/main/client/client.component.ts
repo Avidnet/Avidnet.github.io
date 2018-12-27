be49ebc1743b2677bd55ef8ce643dde11a20e186
Parham Alvani <parham.alvani@gmail.com>
2018-12-27 20:01:26 +0330

    Website is ready.

--- src/app/main/client/client.component.ts
+++ src/app/main/client/client.component.ts
@@ -15,15 +15,19 @@ export class ClientComponent implements OnInit {
 
   @HostBinding('attr.class') elementClasses = 'content has-bg bg-green';
 
-  public clients: Client[] = [{
-    name: 'Joveyn',
-    title: 'CEO',
-    quote: 'Agriculture needs redesign <br> and Avid does it'
-  }, {
-    name: 'Magfa',
-    title: 'IoT Project Director',
-    quote: 'Create platform <br> when you need it'
-  }];
+  public clients: Client[] = [
+    /*
+    {
+      name: 'Joveyn',
+      title: 'CEO',
+      quote: 'Agriculture needs redesign <br> and Avid does it'
+    }, {
+      name: 'Magfa',
+      title: 'IoT Project Director',
+      quote: 'Create platform <br> when you need it'
+    }
+    */
+  ];
 
   constructor() { }
 
