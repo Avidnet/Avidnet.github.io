05563ec31b45f280e2111c6b094f352b03b236c0
Parham Alvani <parham.alvani@gmail.com>
2018-12-27 12:04:05 +0330

    Removes debugging log.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,6 @@ export class AppComponent {
     router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
         const locale = event.url.split('/')[1]; // read the locale part of the current url
-        console.log(locale);
         if (locale && locale === 'fa') {
           translate.use('fa');
         } else {
