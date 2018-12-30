d842bd0a3e147cc7a20697ee2d1917d40debc763
Parham Alvani <parham.alvani@gmail.com>
2018-12-30 19:06:39 +0330

    Registers faniot component.

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -19,6 +19,7 @@ import { MilestoneComponent } from './milestone/milestone.component';
 import { ClientComponent } from './client/client.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { FaniotComponent } from './faniot/faniot.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { GalleryComponent } from './gallery/gallery.component';
     MilestoneComponent,
     ClientComponent,
     PricingComponent,
-    GalleryComponent
+    GalleryComponent,
+    FaniotComponent
   ],
   imports: [
     BrowserModule,
