76b1b6acf0478c193c07386dc1e303b855e434d6
Parham Alvani <parham.alvani@gmail.com>
2018-12-31 16:59:28 +0330

    Better than scrolling.

--- src/app/main/faniot/faniot.component.ts
+++ src/app/main/faniot/faniot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-faniot',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaniotComponent implements OnInit {
 
+  public innerWidth: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
   }
 
 }
